63163a7852635ba82574fca23b67ea3870658233
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-04-30 16:55:19 -0400

    built main index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const parser = require('body-parser')
+const cityController = require('./controllers/City')
+const parkingController =require('./controllers/Parking')
+const barController = require('./controllers/Bar')
+
+
+const app = express()
+
+app.use(parser.urlencoded({extended: true}))
+app.use(parser.json())
+
+app.use('/api/city', cityController)
+app.use('/api/parking', parkingController)
+app.use('/api/bars', barController)
+
+app.listen(3000, () => console.log('NEMO Just keep swimming......on port 3000'))
