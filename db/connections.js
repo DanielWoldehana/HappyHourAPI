7bb70322ae71d42409806bd0f0fdaea9453518bf
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-02 23:43:05 -0400

    attempt 3

--- db/connections.js
+++ db/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://danielwoldehana:ilovemongodb22@cluster2-hlmni.mongodb.net/happyhourDB', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://danielwoldehana:ilovemongodb22!@cluster2-hlmni.mongodb.net/happyhourDB', {useNewUrlParser: true})
 mongoose.Promise = Promise
 
 
